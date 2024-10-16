bbc690ba40e4e2d00744c22f80cc879f497d5242
park <rudwls2717@pusan.ac.kr>
2024-10-16 22:44:14 +0900

    init

--- auto_run_2.py
+++ auto_run_2.py
@@ -15,6 +15,28 @@ def split(s):
     print(params)
     return params
 
+cmd_lst = ['--data ./data/preprocessed/KIBA/random --d1_type graph --d2_type seq --data_name KIBA --project_name KIBA_random_GnS_concat --n_workers 10 --n_epochs 1000 --lr 1e-4 --batch_size 1024 --use_cuda 1 --joint concat',
+           '--data ./data/preprocessed/KIBA/cold_split/Drug --d1_type graph --d2_type seq --data_name KIBA --project_name KIBA_cold_drug_GnS_concat --n_workers 10 --n_epochs 1000 --lr 1e-4 --batch_size 1024 --use_cuda 1 --joint concat',
+           '--data ./data/preprocessed/KIBA/cold_split/Target --d1_type graph --d2_type seq --data_name KIBA --project_name KIBA_cold_target_GnS_concat --n_workers 10 --n_epochs 1000 --lr 1e-4 --batch_size 1024 --use_cuda 1 --joint concat',
+           '--data ./data/preprocessed/KIBA/cold_split/Drug_and_Target --d1_type graph --d2_type seq --data_name KIBA --project_name KIBA_cold_DT_GnS_concat --n_workers 10 --n_epochs 1000 --lr 1e-4 --batch_size 1024 --use_cuda 1 --joint concat',
+
+           '--data ./data/preprocessed/KIBA/random --d1_type seq --d2_type seq --data_name KIBA --project_name KIBA_random_SnS_concat --n_workers 10 --n_epochs 1000 --lr 1e-4 --batch_size 1024 --use_cuda 1 --joint concat',
+           '--data ./data/preprocessed/KIBA/cold_split/Drug --d1_type seq --d2_type seq --data_name KIBA --project_name KIBA_cold_drug_SnS_concat --n_workers 10 --n_epochs 1000 --lr 1e-4 --batch_size 1024 --use_cuda 1 --joint concat',
+           '--data ./data/preprocessed/KIBA/cold_split/Target --d1_type seq --d2_type seq --data_name KIBA --project_name KIBA_cold_target_SnS_concat --n_workers 10 --n_epochs 1000 --lr 1e-4 --batch_size 1024 --use_cuda 1 --joint concat',
+           '--data ./data/preprocessed/KIBA/cold_split/Drug_and_Target --d1_type seq --d2_type seq --data_name KIBA --project_name KIBA_cold_DT_SnS_concat --n_workers 10 --n_epochs 1000 --lr 1e-4 --batch_size 1024 --use_cuda 1 --joint concat'
+           ]
+
+
+import time
+for command in cmd_lst:
+    start = time.time()
+    finish(f'\nstarting {command}\n - server gpu 1')
+    subprocess.run(args=[sys.executable, 'CJRM.py'] + split(command))
+
+    f_time = time.strftime("%H:%M:%S", time.gmtime(time.time() - start))
+    finish(f'\nFinished: {command}\nLearning time: {f_time} - server gpu 1')
+finish(f'\nAll job finished successfully!')
+
 
 # cmd_lst = ['--data ./data/preprocessed/DAVIS/random --d1_type seq --d2_type seq --data_name DAVIS --project_name DAVIS_random_SnS_concat --n_workers 10 --n_epochs 200 --lr 1e-3 --batch_size 1024 --joint concat',
 #            '--data ./data/preprocessed/DAVIS/random --d1_type seq --d2_type graph --data_name DAVIS --project_name DAVIS_random_SnG_concat --n_workers 10 --n_epochs 200 --lr 1e-3 --batch_size 1024 --joint concat',
@@ -36,20 +58,3 @@ def split(s):
 #            '--data ./data/preprocessed/DAVIS/cold_split/Drug_and_Target --d1_type graph --d2_type seq --data_name DAVIS --project_name DAVIS_cold_DT_GnS_concat --n_workers 10 --n_epochs 200 --lr 1e-3 --batch_size 1024 --joint concat',
 #            '--data ./data/preprocessed/DAVIS/cold_split/Drug_and_Target --d1_type graph --d2_type graph --data_name DAVIS --project_name DAVIS_cold_DT_GnG_concat --n_workers 10 --n_epochs 200 --lr 1e-3 --batch_size 1024 --joint concat'
 #            ]
-
-cmd_lst = ['--data ./data/preprocessed/KIBA/random --d1_type graph --d2_type seq --data_name KIBA --project_name KIBA_random_GnS_concat --n_workers 10 --n_epochs 1000 --lr 1e-4 --batch_size 1024 --use_cuda 1 --joint concat',
-           '--data ./data/preprocessed/KIBA/cold_split/Drug --d1_type graph --d2_type seq --data_name KIBA --project_name KIBA_cold_drug_GnS_concat --n_workers 10 --n_epochs 1000 --lr 1e-4 --batch_size 1024 --use_cuda 1 --joint concat',
-           '--data ./data/preprocessed/KIBA/cold_split/Target --d1_type graph --d2_type seq --data_name KIBA --project_name KIBA_cold_target_GnS_concat --n_workers 10 --n_epochs 1000 --lr 1e-4 --batch_size 1024 --use_cuda 1 --joint concat',
-           '--data ./data/preprocessed/KIBA/cold_split/Drug_and_Target --d1_type graph --d2_type seq --data_name KIBA --project_name KIBA_cold_DT_GnS_concat --n_workers 10 --n_epochs 1000 --lr 1e-4 --batch_size 1024 --use_cuda 1 --joint concat',
-           ]
-
-
-import time
-for command in cmd_lst:
-    start = time.time()
-    finish(f'\nstarting {command}\n - server gpu 1')
-    subprocess.run(args=[sys.executable, 'CJRM.py'] + split(command))
-
-    f_time = time.strftime("%H:%M:%S", time.gmtime(time.time() - start))
-    finish(f'\nFinished: {command}\nLearning time: {f_time} - server gpu 1')
-finish(f'\nAll job finished successfully!')
