8c96863b86104d31442e7381723dc22906556af3
park <rudwls2717@pusan.ac.kr>
2024-10-22 16:14:48 +0900

    init

--- auto_run_2.py
+++ auto_run_2.py
@@ -16,10 +16,10 @@ def split(s):
     return params
 
 
-cmd_lst = ['--data ./data/preprocessed/DAVIS/cold_split/Target --d1_type graph --d2_type seq --data_name DAVIS --project_name DAVIS_cold_target_GnS_ban --joint bi_att --use_cuda 1 --virtual_node --n_workers 4 --batch_size 256',
-           '--data ./data/preprocessed/KIBA/random --d1_type graph --d2_type seq --data_name KIBA --project_name KIBA_random_GnS_ban --joint bi_att --one_shot --virtual_node --n_workers 4 --batch_size 128 --use_cuda 1',
+cmd_lst = ['--data ./data/preprocessed/DAVIS/cold_split/Target --d1_type graph --d2_type seq --data_name DAVIS --project_name DAVIS_cold_target_GnS_ban --joint bi_att --virtual_node --n_workers 4 --batch_size 128 --one_shot'
            ]
 
+# '--data ./data/preprocessed/KIBA/random --d1_type graph --d2_type seq --data_name KIBA --project_name KIBA_random_GnS_ban --joint bi_att --one_shot --virtual_node --n_workers 4 --batch_size 128 --use_cuda 1',
 
 import time
 for command in cmd_lst:
