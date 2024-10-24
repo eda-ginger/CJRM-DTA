deca50286332f75c8695bae791f1cfa168584ab1
park <rudwls2717@pusan.ac.kr>
2024-10-24 20:50:08 +0900

    init

---
+++ auto_23-1.py
@@ -0,0 +1,31 @@
+import sys
+import shlex
+import subprocess
+from knockknock import discord_sender
+
+# notice discord
+webhook_url = "https://discord.com/api/webhooks/1009749385170665533/m4nldXOXR5f9iWaXoCDLNGhNI48XEpy-Y9CcBpdFJW_xUipS54LCzXX9xZaCY6IH0vSl"
+@discord_sender(webhook_url=webhook_url)
+def finish(message):
+    return message  # Optional return value
+
+
+def split(s):
+    params = shlex.split(s)
+    print(params)
+    return params
+
+cuda_num = 1
+cmd_lst = [f"--data ./data/preprocessed/DAVIS/cold_split/Drug --d1_type graph --d2_type seq --data_name DAVIS --project_name GraphDTA_CD_MHA-Cross_16-1 --virtual_node --joint cross_att --use_cuda {cuda_num}",
+           f"--data ./data/preprocessed/DAVIS/cold_split/Target --d1_type graph --d2_type seq --data_name DAVIS --project_name GraphDTA_CT_MHA-Cross_16-1 --virtual_node --joint cross_att --use_cuda {cuda_num}"
+           ]
+
+import time
+for command in cmd_lst:
+    start = time.time()
+    finish(f'\nstarting {command}\n - server 23-1')
+    subprocess.run(args=[sys.executable, 'CJRM.py'] + split(command))
+
+    f_time = time.strftime("%H:%M:%S", time.gmtime(time.time() - start))
+    finish(f'\nFinished: {command}\nLearning time: {f_time} - server 23-1')
+finish(f'\nAll job finished successfully!')
